fd638175d7a421534cef71f5004075b8ce2a7473
ntan-vixi <33009981+ntan-vixi@users.noreply.github.com>
2017-11-23 12:57:31 +0400

    Inititating program

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//ROUTES
+app.get('/', function(req, res) {
+	res.send("Chatbot running...")
+})
+
+//Twitter
+app.get('/webhook/twitter', function(req, res) {
+
+})
+
+app.listen(app.get('port'), function() {
+	console.log("running: " + app.get('port'))
+})
